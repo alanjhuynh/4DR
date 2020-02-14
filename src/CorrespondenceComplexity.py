ed7a2003c783d33c4ca77d83d78266fd52e5c048
Jesse Angelo <pangiljac@gmail.com>
2020-02-14 12:04:29 -0800

    Small Changes and tests

--- src/CorrespondenceComplexity.py
+++ src/CorrespondenceComplexity.py
@@ -4,14 +4,16 @@ import time
 import os, os.path
 
 def Compare(original, new):
-    originalSize = original.size
-    newSize = new.size
-    print(originalSize, newSize)
-    print(original)
-    print(new)
+    originalSize = np.size(original, 0)
+    newSize = np.size(new, 0)
     if(originalSize != newSize):
-        print("Size")
         return False
+    for n in range(0, newSize, 1):
+        if(not(original[n][0] == new[n][0])):
+            return False
+        if(not(original[n][1] == new[n][1])):
+            return False
+    
     return True
 
 while(True):
@@ -35,78 +37,88 @@ imagePoints2 = np.array([[-1.0, -1.0]])
 gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
 gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 N = 0
-h = 40
-w = 40
+h = np.size(frame1, 0)
+print(h)
+w = np.size(frame1, 1)
+W = np.size(frame1, 1)
+print(W)
 
 
-start_time = time.time()
-for y in range(windowEnd, h + windowStart, 1):
-    for x in range(windowEnd, w + windowStart, 1):
-        
-        n = 0
-        for i in range(windowStart, windowEnd + 1, 1):
-            for j in range(windowStart, windowEnd + 1, 1):
-                window[n] = gray1[y+i][x+j]
-                n += 1
-        mean = np.mean(window)
-        n = 0
-        for i in range(windowStart, windowEnd + 1, 1):
-            for j in range(windowStart, windowEnd + 1, 1):
-                window[n] = window[n] - mean
-                n += 1
-        magnitude = np.linalg.norm(window)
+comparer = input("Original?")
 
-        maxValue = 0
-        bestN = -1
-        for n in range (windowEnd, w + windowStart, 1):
-            k = 0
+if(comparer == "True"):
+
+    start_time = time.time()
+    for y in range(windowEnd, h + windowStart, 1):
+        for x in range(windowEnd, w + windowStart, 1):
+            
+            n = 0
             for i in range(windowStart, windowEnd + 1, 1):
                 for j in range(windowStart, windowEnd + 1, 1):
-                    windowPrime[k] = gray2[y+i][n+j]
-                    k += 1
-            mean = np.mean(windowPrime)  
-            k = 0
+                    window[n] = gray1[y+i][x+j]
+                    n += 1
+            mean = np.mean(window)
+            n = 0
             for i in range(windowStart, windowEnd + 1, 1):
                 for j in range(windowStart, windowEnd + 1, 1):
-                    windowPrime[k] = windowPrime[k] - mean
-                    k += 1
-            magnitudePrime = np.linalg.norm(windowPrime)
+                    window[n] = window[n] - mean
+                    n += 1
+            magnitude = np.linalg.norm(window)
 
-            if(magnitudePrime * magnitude == 0):
-                continue
+            maxValue = 0
+            bestN = -1
+            for n in range (windowEnd, w + windowStart, 1):
+                k = 0
+                for i in range(windowStart, windowEnd + 1, 1):
+                    for j in range(windowStart, windowEnd + 1, 1):
+                        windowPrime[k] = gray2[y+i][n+j]
+                        k += 1
+                mean = np.mean(windowPrime)  
+                k = 0
+                for i in range(windowStart, windowEnd + 1, 1):
+                    for j in range(windowStart, windowEnd + 1, 1):
+                        windowPrime[k] = windowPrime[k] - mean
+                        k += 1
+                magnitudePrime = np.linalg.norm(windowPrime)
 
-            testValue = np.dot(window, windowPrime)/(magnitude * magnitudePrime)
-            if(maxValue<testValue):
-                bestN = n
-                maxValue = testValue
+                if(magnitudePrime * magnitude == 0):
+                    continue
 
-        if(maxValue > threshold):
-            if(N == 0):
-                #MAKE SURE THIS IS RIGHT (X is 0 and Y is 1)
-                imagePoints1[N][0] = float(x)
-                imagePoints1[N][1] = float(y)
-                imagePoints2[N][0] = float(bestN)
-                imagePoints2[N][1] = float(y)
-                N += 1
-            else:
-                np.append(imagePoints1, [[float(x),float(y)]], axis = 0)
-                np.append(imagePoints2, [[float(bestN),float(y)]], axis = 0)
+                testValue = np.dot(window, windowPrime)/(magnitude * magnitudePrime)
+                if(maxValue<testValue):
+                    bestN = n
+                    maxValue = testValue
 
-end_time = time.time()
+            if(maxValue > threshold):
+                if(N == 0):
+                    #MAKE SURE THIS IS RIGHT (X is 0 and Y is 1)
+                    imagePoints1[N][0] = float(x)
+                    imagePoints1[N][1] = float(y)
+                    imagePoints2[N][0] = float(bestN)
+                    imagePoints2[N][1] = float(y)
+                    N += 1
+                else:
+                    imagePoints1 = np.append(imagePoints1, [[float(x),float(y)]], axis = 0)
+                    imagePoints2 = np.append(imagePoints2, [[float(bestN),float(y)]], axis = 0)
 
-print("Current Process: " + str(end_time - start_time) + " Seconds")
+    end_time = time.time()
 
+    print("Current Process: " + str(end_time - start_time) + " Seconds")
+
+################ TEST ONE ##################
+
+print(h - (2*windowEnd))
 
 multTestPoints_1 = np.array([[-1.0, -1.0]])
 multTestPoints_2 = np.array([[-1.0, -1.0]])
-multArray = np.zeros((w, h, w)) #Depth Dimension, Rows, Columns
-magPrimeArray = np.zeros((w - (2*windowEnd), h - (2*windowEnd)))
-winPrimeArray = np.zeros((w - (2*windowEnd), h - (2*windowEnd), windowSize*windowSize))
+#multArray = np.zeros((w, h, w)) #Depth Dimension, Rows, Columns
+magPrimeArray = np.zeros((h - (2*windowEnd),W - (2*windowEnd)))
+winPrimeArray = np.zeros((h - (2*windowEnd),W - (2*windowEnd), windowSize*windowSize))
 N = 0
 
 start_time = time.time()
 for y in range(windowEnd, h + windowStart, 1):
-    for n in range (windowEnd, w + windowStart, 1):
+    for n in range (windowEnd, W + windowStart, 1):
         k = 0
         for i in range(windowStart, windowEnd + 1, 1):
             for j in range(windowStart, windowEnd + 1, 1):
@@ -141,7 +153,7 @@ for y in range(windowEnd, h + windowStart, 1):
         maxValue = 0
         bestN = -1
         
-        for n in range (windowEnd, w + windowStart, 1):
+        for n in range (windowEnd, W + windowStart, 1):
             
             data = magPrimeArray[y - windowEnd][n - windowEnd] * magnitude
             if(data == 0):
@@ -161,15 +173,24 @@ for y in range(windowEnd, h + windowStart, 1):
                 multTestPoints_2[N][1] = float(y)
                 N += 1
             else:
-                print("APPEND " + str(x) + ", " + str(y) )
-                np.append(multTestPoints_1, [[float(x),float(y)]], axis = 1)
-                print(multTestPoints_1)
-                np.append(multTestPoints_2, [[float(bestN),float(y)]], axis = 1)
+                multTestPoints_1 = np.append(multTestPoints_1, [[float(x),float(y)]], axis = 0)
+                multTestPoints_2 = np.append(multTestPoints_2, [[float(bestN),float(y)]], axis = 0)
 
 end_time = time.time()
+if(comparer == "True"):
+    compareBool = Compare(imagePoints1, multTestPoints_1)
+    if(not compareBool):
+        print("Test Failed")
+    else:
+        print("Success")
 
-compareBool = Compare(imagePoints1, multTestPoints_1)
-if(not compareBool):
-    print("Test Failed")
+print("Current Process: " + str(end_time - start_time) + " Seconds")
 
-print("Current Process: " + str(end_time - start_time) + " Seconds")  
+################ TEST TWO ##################
+
+condTestPoints_1 = np.array([[-1.0, -1.0]])
+condTestPoints_2 = np.array([[-1.0, -1.0]])
+multArray = np.zeros((w, h, w)) #Depth Dimension, Rows, Columns
+magPrimeArray = np.zeros((w - (2*windowEnd), h - (2*windowEnd)))
+winPrimeArray = np.zeros((w - (2*windowEnd), h - (2*windowEnd), windowSize*windowSize))
+N = 0
